90e830c9b61406a053932d73ee83fc61629d175f
priscilla goessens <priscillagoessens@gmail.com>
2023-10-18 19:06:04 +0200

    eventlistener + bullet points

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,27 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let arrow_left = document.getElementById("arrow_left");
+let arrow_right = document.getElementById("arrow_right");
+
+arrow_left.addEventListener("click", (event) => {
+	console.log("Vous avez cliqué sur le bouton")
+});
+arrow_right.addEventListener("click", (event) => {
+	console.log("Vous avez cliqué sur le bouton droit")
+});
+
+
+slides.forEach((e) =>{
+	dot = document.createElement('div');
+	dot.className = "dot"
+	document.getElementsByClassName('dots')[0].appendChild(dot);
+	
+})
+document.querySelector('.dots :first-child').classList.add('dot_selected')
+
+
+
+
+
