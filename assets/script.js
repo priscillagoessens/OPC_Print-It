46ecf28b7d07d0b6e0f99e9333fbff5b6c3093c1
priscilla goessens <priscillagoessens@gmail.com>
2023-10-31 16:57:43 +0100

    etape 4

--- assets/script.js
+++ assets/script.js
@@ -17,23 +17,48 @@ const slides = [
 	}
 ]
 
-let arrow_left = document.getElementById("arrow_left");
-let arrow_right = document.getElementById("arrow_right");
+const arrow_left = document.querySelector('.arrow_left');
+const arrow_right = document.querySelector('.arrow_right');
+const bullets =  document.getElementsByClassName('dot')
+const image = document.querySelector('.banner-img');
+const slideText = document.getElementById("text");
+
+arrow_right.addEventListener("click", (event) => {
+	// Parcour les éléments de la liste "bullets"
+	for(i=0; i < bullets.length; i++){
+		//verifie si il la classe est presente
+		if(bullets[i].classList.contains('dot_selected')){
+			//si oui, on supprime
+			bullets[i].classList.remove('dot_selected')
+			//on ajoute la classe pour le passer en mode actif sur le bullet/dot
+			bullets[i+1].classList.add('dot_selected')
+			//arrete la boucle
+			break;
+		}
+	}
+	image.src = "./assets/images/slideshow/" + slides[i+1].image;
+	slideText.innerHTML = slides[i+1].tagLine;
+})
 
 arrow_left.addEventListener("click", (event) => {
-	console.log("Vous avez cliqué sur le bouton")
+	for(i=0; i < bullets.length; i++){
+		if(bullets[i].classList.contains('dot_selected')){
+			bullets[i].classList.remove('dot_selected')
+			bullets[i-1].classList.add('dot_selected')
+			break;
+		}
+	}
+	image.src = "./assets/images/slideshow/" + slides[i-1].image;
+	slideText.innerHTML = slides[i-1].tagLine;
 });
-arrow_right.addEventListener("click", (event) => {
-	console.log("Vous avez cliqué sur le bouton droit")
-});
-
-
-slides.forEach((e) =>{
-	dot = document.createElement('div');
-	dot.className = "dot"
-	document.getElementsByClassName('dots')[0].appendChild(dot);
 	
-})
+for (let i in slides) {
+	//creer une div pour chaque dot en fonction du nb d'objet
+	const dot = document.createElement('div');
+	dot.className = "dot"
+	document.getElementsByClassName('dots')[0].appendChild(dot);	
+}
+
 document.querySelector('.dots :first-child').classList.add('dot_selected')
 
 
